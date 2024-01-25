af77e1502193d4f8d2e03280aaf7605c6ef03406
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2024-01-25 11:59:29 +0200

    done with basic coursel data

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import {availableOpportunities} from "../data"
 
+
+//// each opportunity card
+const card = availableOpportunities.map((opportunity)=>{
+    const {title,id} =  opportunity
+    return <h2 key={id} >{title}</h2>
+})
 
 const Hero = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+      };
+
   return (
-    <div>Hero</div>
+    <Slider {...settings}>
+      {card}
+    </Slider>
   )
 }
 
 export default Hero
+
+
 
 
 
---
+++ client/src/data.jsx
@@ -0,0 +1,18 @@
+import { nanoid } from 'nanoid';
+import react from "react"
+import study1 from "./assets/images/study1.jpg"
+//////
+export const availableOpportunities = [{
+    id:nanoid(),
+    title:"ALU",
+    //img: study1,
+    //href:"https://www.coursera.org/account/accomplishments/verify/L7WCHFTPXZAD"
+  },
+  {
+    id:nanoid(),
+    title:"BBC",
+   // img: "",
+    //href:"https://www.coursera.org/account/accomplishments/verify/L7WCHFTPXZAD"
+  }
+
+]
