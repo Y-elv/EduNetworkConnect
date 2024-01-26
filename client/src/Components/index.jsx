5a39119fdc4b177d6ab8fa4651681b6b4d8c5ca0
Y-elv <mugishaelvis456@gmail.com>
2024-01-26 17:01:21 +0200

    ending of footer

--- client/src/Components/index.jsx
+++ client/src/Components/index.jsx
@@ -1,5 +1,6 @@
 import BecomeMember from "./BecomeMember";
 import Header from "./Header";
 import Hero from "./Hero";
+import Footer from "./Footer";
 
- export {Header,Hero,BecomeMember} 
+ export {Header,Hero,BecomeMember,Footer} 
